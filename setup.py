91c68fb63332c41b2f70634273a78a0671589bee
Shay Sakazi <shaysakazi@Shays-MBP.lan>
2020-05-03 17:39:23 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='cert r4.1',
+    author='Shay Sakazi',
+    license='',
+)
